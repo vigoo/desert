5099fe6648ad225ca7fa41755080dd893fbb32fa
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-06-23 23:23:01 +0200

    Fix deployment key

--- scripts/decrypt_keys.sh
+++ scripts/decrypt_keys.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
-openssl aes-256-cbc -K $encrypted_aa8e06d836e3_key -iv $encrypted_aa8e06d836e3_iv -in travis-deploy-key.enc -out travis-deploy-key -d
+openssl aes-256-cbc -K $encrypted_05f822b6caa8_key -iv $encrypted_05f822b6caa8_iv -in travis-deploy-key.enc -out travis-deploy-key -d
 chmod 600 travis-deploy-key
 cp travis-deploy-key ~/.ssh/id_rsa
