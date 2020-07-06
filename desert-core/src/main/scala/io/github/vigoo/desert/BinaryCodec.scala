9fd4588fa684a9713b18ffc9c62b4e76fc765168
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-07-06 10:13:32 +0200

    Code reorganization

--- desert-core/src/main/scala/io/github/vigoo/desert/BinaryCodec.scala
+++ desert-core/src/main/scala/io/github/vigoo/desert/BinaryCodec.scala
@@ -2,12 +2,8 @@ package io.github.vigoo.desert
 
 import cats.data.{ReaderT, StateT}
 import cats.instances.either._
-import cats.syntax.flatMap._
-import io.github.vigoo.desert.BinaryDeserializer.{Deser, DeserializationEnv}
-import io.github.vigoo.desert.BinarySerializer.{Ser, SerializationEnv}
-import io.github.vigoo.desert.SerializerState.{RefAlreadyStored, RefId, RefIsNew, StoreRefResult, StoreStringResult, StringId}
-import io.github.vigoo.desert.TypeRegistry.RegisteredTypeId
-import shapeless.Lazy
+import io.github.vigoo.desert.BinaryDeserializer.Deser
+import io.github.vigoo.desert.BinarySerializer.Ser
 
 import scala.language.experimental.macros
 import scala.reflect.ClassTag
@@ -31,63 +27,6 @@ object BinarySerializer {
   }
 }
 
-trait BinarySerializerOps {
-  final def getOutput: Ser[BinaryOutput] = ReaderT.ask[StateT[Either[DesertFailure, *], SerializerState, *], SerializationEnv].map(_.output)
-  final def getOutputTypeRegistry: Ser[TypeRegistry] = ReaderT.ask[StateT[Either[DesertFailure, *], SerializerState, *], SerializationEnv].map(_.typeRegistry)
-  final def getSerializerState: Ser[SerializerState] = ReaderT.liftF(StateT.get[Either[DesertFailure, *], SerializerState])
-  final def setSerializerState(state: SerializerState): Ser[Unit] = ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](state))
-
-  final def writeByte(value: Byte): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeByte(value)))
-  final def writeShort(value: Short): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeShort(value)))
-  final def writeInt(value: Int): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeInt(value)))
-  final def writeVarInt(value: Int, optimizeForPositive: Boolean): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeVarInt(value, optimizeForPositive)))
-  final def writeLong(value: Long): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeLong(value)))
-  final def writeFloat(value: Float): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeFloat(value)))
-  final def writeDouble(value: Double): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeDouble(value)))
-  final def writeBytes(value: Array[Byte]): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeBytes(value)))
-
-  final def writeUnknown(value: Any): Ser[Unit] =
-    getOutputTypeRegistry.flatMap { typeRegistry =>
-      typeRegistry.get(value) match {
-        case Some(registration) =>
-          for {
-            _ <- writeVarInt(registration.id.value, optimizeForPositive = true)
-            _ <- registration.serialize(value)
-          } yield ()
-        case None =>
-          failSerializerWith(TypeNotRegistered(value.getClass))
-      }
-    }
-
-  final def write[U : BinarySerializer](value: U): Ser[Unit] = implicitly[BinarySerializer[U]].serialize(value)
-
-  final def finishSerializer(): Ser[Unit] = finishSerializerWith(())
-  final def finishSerializerWith[T](value: T): Ser[T] = Ser.fromEither(Right(value))
-  final def failSerializerWith(failure: DesertFailure): Ser[Unit] = Ser.fromEither(Left(failure))
-
-  final def storeString(value: String): Ser[StoreStringResult] =
-    for {
-      state <- getSerializerState
-      (newState, result) = state.storeString(value)
-      _ <- ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](newState))
-    } yield result
-
-  final def storeRef(value: AnyRef): Ser[StoreRefResult] =
-    for {
-      state <- getSerializerState
-      (newState, result) = state.storeRef(value)
-      _ <- ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](newState))
-    } yield result
-
-  final def storeRefOrObject[T <: AnyRef](value: T)(implicit codec: Lazy[BinaryCodec[T]]): Ser[Unit] =
-    storeRef(value).flatMap {
-      case RefAlreadyStored(id) => writeVarInt(id.value, optimizeForPositive = true)
-      case RefIsNew(_) => writeVarInt(0, optimizeForPositive = true) >> write(value)(codec.value)
-    }
-}
-
-object BinarySerializerOps extends BinarySerializerOps
-
 trait BinaryDeserializer[T] { self =>
   import BinaryDeserializer._
 
@@ -105,76 +44,6 @@ object BinaryDeserializer {
     final def fromEither[T](value: Either[DesertFailure, T]): Deser[T] = ReaderT.liftF(StateT.liftF(value))
   }
 }
-
-trait BinaryDeserializerOps {
-  final def getInput: Deser[BinaryInput] = ReaderT.ask[StateT[Either[DesertFailure, *], SerializerState, *], DeserializationEnv].map(_.input)
-  final def getInputTypeRegistry: Deser[TypeRegistry] = ReaderT.ask[StateT[Either[DesertFailure, *], SerializerState, *], DeserializationEnv].map(_.typeRegistry)
-  final def getDeserializerState: Deser[SerializerState] = ReaderT.liftF(StateT.get[Either[DesertFailure, *], SerializerState])
-  final def setDeserializerState(state: SerializerState): Deser[Unit] = ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](state))
-
-  final def readByte(): Deser[Byte] = getInput.flatMap(input => Deser.fromEither(input.readByte()))
-  final def readShort(): Deser[Short] = getInput.flatMap(input => Deser.fromEither(input.readShort()))
-  final def readInt(): Deser[Int] = getInput.flatMap(input => Deser.fromEither(input.readInt()))
-  final def readVarInt(optimizeForPositive: Boolean): Deser[Int] = getInput.flatMap(input => Deser.fromEither(input.readVarInt(optimizeForPositive)))
-  final def readLong(): Deser[Long] = getInput.flatMap(input => Deser.fromEither(input.readLong()))
-  final def readFloat(): Deser[Float] = getInput.flatMap(input => Deser.fromEither(input.readFloat()))
-  final def readDouble(): Deser[Double] = getInput.flatMap(input => Deser.fromEither(input.readDouble()))
-  final def readBytes(count: Int): Deser[Array[Byte]] = getInput.flatMap(input => Deser.fromEither(input.readBytes(count)))
-  final def read[T: BinaryDeserializer](): Deser[T] = implicitly[BinaryDeserializer[T]].deserialize()
-
-  final def readUnknown(): Deser[Any] =
-    for {
-      typeRegistry <- getInputTypeRegistry
-      typeId <- readVarInt(optimizeForPositive = true).map(RegisteredTypeId)
-      result <- typeRegistry.forId(typeId) match {
-        case Some(registration) =>
-          registration.codec.deserialize()
-        case None =>
-          failDeserializerWith(InvalidTypeId(typeId))
-      }
-    } yield result
-
-  final def finishDeserializerWith[T](value: T): Deser[T] = Deser.fromEither(Right(value))
-  final def failDeserializerWith[T](failure: DesertFailure): Deser[T] = Deser.fromEither(Left(failure))
-
-  final def getString(value: StringId): Deser[Option[String]] =
-    for {
-      state <- getDeserializerState
-    } yield state.stringsById.get(value)
-
-  final def storeReadString(value: String): Deser[Unit] =
-    for {
-      state <- ReaderT.liftF(StateT.get[Either[DesertFailure, *], SerializerState])
-      (newState, _) = state.storeString(value)
-      _ <- ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](newState))
-    } yield ()
-
-  final def getRef(value: RefId): Deser[Option[AnyRef]] =
-    for {
-      state <- getDeserializerState
-    } yield state.refsById.get(value)
-
-  final def storeReadRef(value: AnyRef): Deser[Unit] =
-    for {
-      state <- ReaderT.liftF(StateT.get[Either[DesertFailure, *], SerializerState])
-      (newState, r) = state.storeRef(value)
-      _ <- ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](newState))
-    } yield ()
-
-  def readRefOrValue[T <: AnyRef](storeReadReference: Boolean = true)(implicit codec: Lazy[BinaryCodec[T]]): Deser[T] =
-    readVarInt(optimizeForPositive = true).flatMap {
-      case 0 => for {
-        value <- read[T]()(codec.value)
-        _ <- if (storeReadReference) storeReadRef(value) else finishDeserializerWith(())
-      } yield value
-      case id => getRef(RefId(id)).flatMap {
-        case None => failDeserializerWith(InvalidRefId(RefId(id)))
-        case Some(value) => finishDeserializerWith(value.asInstanceOf[T])
-      }
-    }
-}
-
-object BinaryDeserializerOps extends BinaryDeserializerOps
 
 trait BinaryCodec[T] extends BinarySerializer[T] with BinaryDeserializer[T]
 
---
+++ desert-core/src/main/scala/io/github/vigoo/desert/BinarySerializerOps.scala
@@ -0,0 +1,65 @@
+package io.github.vigoo.desert
+
+import cats.data.{ReaderT, StateT}
+import cats.instances.either._
+import cats.syntax.flatMap._
+import io.github.vigoo.desert.BinarySerializer.{Ser, SerializationEnv}
+import io.github.vigoo.desert.SerializerState.{RefAlreadyStored, RefIsNew, StoreRefResult, StoreStringResult}
+import shapeless.Lazy
+
+trait BinarySerializerOps {
+  final def getOutput: Ser[BinaryOutput] = ReaderT.ask[StateT[Either[DesertFailure, *], SerializerState, *], SerializationEnv].map(_.output)
+  final def getOutputTypeRegistry: Ser[TypeRegistry] = ReaderT.ask[StateT[Either[DesertFailure, *], SerializerState, *], SerializationEnv].map(_.typeRegistry)
+  final def getSerializerState: Ser[SerializerState] = ReaderT.liftF(StateT.get[Either[DesertFailure, *], SerializerState])
+  final def setSerializerState(state: SerializerState): Ser[Unit] = ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](state))
+
+  final def writeByte(value: Byte): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeByte(value)))
+  final def writeShort(value: Short): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeShort(value)))
+  final def writeInt(value: Int): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeInt(value)))
+  final def writeVarInt(value: Int, optimizeForPositive: Boolean): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeVarInt(value, optimizeForPositive)))
+  final def writeLong(value: Long): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeLong(value)))
+  final def writeFloat(value: Float): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeFloat(value)))
+  final def writeDouble(value: Double): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeDouble(value)))
+  final def writeBytes(value: Array[Byte]): Ser[Unit] = getOutput.flatMap(output => Ser.fromEither(output.writeBytes(value)))
+
+  final def writeUnknown(value: Any): Ser[Unit] =
+    getOutputTypeRegistry.flatMap { typeRegistry =>
+      typeRegistry.get(value) match {
+        case Some(registration) =>
+          for {
+            _ <- writeVarInt(registration.id.value, optimizeForPositive = true)
+            _ <- registration.serialize(value)
+          } yield ()
+        case None =>
+          failSerializerWith(TypeNotRegistered(value.getClass))
+      }
+    }
+
+  final def write[U : BinarySerializer](value: U): Ser[Unit] = implicitly[BinarySerializer[U]].serialize(value)
+
+  final def finishSerializer(): Ser[Unit] = finishSerializerWith(())
+  final def finishSerializerWith[T](value: T): Ser[T] = Ser.fromEither(Right(value))
+  final def failSerializerWith(failure: DesertFailure): Ser[Unit] = Ser.fromEither(Left(failure))
+
+  final def storeString(value: String): Ser[StoreStringResult] =
+    for {
+      state <- getSerializerState
+      (newState, result) = state.storeString(value)
+      _ <- ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](newState))
+    } yield result
+
+  final def storeRef(value: AnyRef): Ser[StoreRefResult] =
+    for {
+      state <- getSerializerState
+      (newState, result) = state.storeRef(value)
+      _ <- ReaderT.liftF(StateT.set[Either[DesertFailure, *], SerializerState](newState))
+    } yield result
+
+  final def storeRefOrObject[T <: AnyRef](value: T)(implicit codec: Lazy[BinaryCodec[T]]): Ser[Unit] =
+    storeRef(value).flatMap {
+      case RefAlreadyStored(id) => writeVarInt(id.value, optimizeForPositive = true)
+      case RefIsNew(_) => writeVarInt(0, optimizeForPositive = true) >> write(value)(codec.value)
+    }
+}
+
+object BinarySerializerOps extends BinarySerializerOps
