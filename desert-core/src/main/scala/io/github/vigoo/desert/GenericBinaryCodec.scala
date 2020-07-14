612437c85fdf53af36be00146472e866933ce8c5
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-07-14 15:55:31 +0200

    Some documentation of the generic binary codec

--- desert-core/src/main/scala/io/github/vigoo/desert/GenericBinaryCodec.scala
+++ desert-core/src/main/scala/io/github/vigoo/desert/GenericBinaryCodec.scala
@@ -18,13 +18,21 @@ import shapeless.tag._
 
 import scala.reflect.ClassTag
 
+/**
+ * Trait containing the more generic implicits backing up the generic binary codec
+ *
+ * These are the default implementations while in [[GenericDerivationApi]] there are some more specialized ones
+ * for handling optional and transient fields / constructors.
+ */
 trait LowerPriorityGenericDerivationApi {
   implicit def hlistSerializer[K <: Symbol, H, T <: HList](implicit witness: Witness.Aux[K],
                                                            headCodec: Lazy[BinaryCodec[H]],
                                                            tailCodec: ChunkedBinarySerializer[T]): ChunkedBinarySerializer[FieldType[K, H] :: T]
+
   implicit def hlistDeserializer[K <: Symbol, H, T <: HList](implicit witness: Witness.Aux[K],
                                                              headCodec: Lazy[BinaryCodec[H]],
                                                              tailCodec: ChunkedBinaryDeserializer[T]): ChunkedBinaryDeserializer[FieldType[K, H] :: T]
+
   implicit def clistSerializer[K <: Symbol, H, T <: Coproduct](implicit witness: Witness.Aux[K],
                                                                headCodec: Lazy[BinaryCodec[H]],
                                                                tailCodec: ChunkedBinarySerializer[T]): ChunkedBinarySerializer[FieldType[K, H] :+: T]
@@ -34,6 +42,17 @@ trait LowerPriorityGenericDerivationApi {
                                                                  tailCodec: ChunkedBinaryDeserializer[T]): ChunkedBinaryDeserializer[FieldType[K, H] :+: T]
 }
 
+/**
+ * Trait containing the public interface for the generic binary codec.
+ *
+ * As there is no single global generic derivation implementation because it is parameteric with the evolution steps,
+ * a specific type's codec derviation is possible by accessing the necessary implicits (for HLists and Coproducts) via
+ * this interface.
+ *
+ * The two type classes implemented for both products and coproducts are [[ChunkedBinarySerializer]] and
+ * [[ChunkedBinaryDeserializer]]. These monads are more specific than the [[BinarySerializer]] and [[BinaryDeserializer]],
+ * holding state specific for a single evolvable value's serialization.
+ */
 trait GenericDerivationApi extends LowerPriorityGenericDerivationApi {
   implicit val hnilSerializer: ChunkedBinarySerializer[HNil]
   implicit val hnilDeserializer: ChunkedBinaryDeserializer[HNil]
@@ -58,13 +77,41 @@ trait GenericDerivationApi extends LowerPriorityGenericDerivationApi {
   implicit def clistTransientDeserializer[K <: Symbol, H, T <: Coproduct](implicit witness: Witness.Aux[K],
                                                                           tailCodec: ChunkedBinaryDeserializer[T]): ChunkedBinaryDeserializer[FieldType[K, MarkedAsTransient[H]] :+: T]
 
+  /**
+   * Type class to tag a labelled generic representation of a type with a type level information about whether each
+   * field or constructor is marked as transient or not.
+   *
+   * For products 'H' is a [[HList]], for coproducts it is a [[Coproduct]], with each item being tagged with the
+   * field or constructor's name by [[LabelledGeneric]], having a type of 'FieldType[K, V]'.
+   *
+   * The result of this type-level operation, 'Result' is a [[HList]] or [[Coproduct]] where those fields or constructors
+   * that are having a transient attribute of [[TransientField]] or [[TransientConstructor]] are also tagged with
+   * the [[MarkedAsTransient]] tag, so each element is either 'FieldType[K, V]' or 'FieldType[K, MarkedAsTransient[V]]'.
+   *
+   * This makes it possible to differentiate in compile time between transient and non-transient flags in the implementation
+   * of the [[ChunkedBinaryDeserializer]] and [[ChunkedBinarySerializer]] type classes.
+   *
+   * @tparam H The generic representation of the type
+   * @tparam AF [[HList]] of the [[TransientField]] annotations, extracted by [[Annotations]]
+   * @tparam AC [[HList]] of the [[TransientConstructor]] annotations, extracted by [[Annotations]]
+   */
   trait TagTransients[H, AF, AC] {
     type Result
 
+    /**
+     * Create a representation of the labelled generic value that also has transient tags
+     */
     def tag(value: H): Result
+
+    /**
+     * Removes the transient tags from a labelled generic representation
+     */
     def untag(value: Result): H
   }
 
+  /**
+   * Implementation of [[TagTransients]] for both products and coproducts
+   */
   object TagTransients {
     type Aux[H, AF, AC, R] = TagTransients[H, AF, AC] { type Result = R }
 
@@ -145,12 +192,22 @@ trait GenericDerivationApi extends LowerPriorityGenericDerivationApi {
       }
   }
 
+  /**
+   * Type class extracting the field names or constructor names of a labelled generic representation to a [[HList]] of [[Symbol]] values
+   * @tparam H The [[LabelledGeneric]] representation of a type
+   */
   trait Symbols[H] {
     type Result <: HList
 
+    /**
+     * Extracts the [[HList]] of [[Symbol]] values from the type-level labelled generic representation
+     */
     def apply(): Result
   }
 
+  /**
+   * Implementation of the [[Symbols]] type class for both products and coproducts
+   */
   object Symbols {
     type Aux[H, R <: HList] = Symbols[H] { type Result = R }
 
@@ -179,7 +236,19 @@ trait GenericDerivationApi extends LowerPriorityGenericDerivationApi {
       }
   }
 
-  trait ToConstructorMap[T] { val constructors: Vector[String] }
+  /**
+   * Type class to extract the type-level generic constructor names to a run-time vector of strings
+   * @tparam T The [[LabelledGeneric]] representation of a type tagged by [[TagTransients]]
+   */
+  trait ToConstructorMap[T] {
+    val constructors: Vector[String]
+  }
+
+  /**
+   * Lower priority implicits for the implementation of [[ToConstructorMap]].
+   *
+   * This enables capturing the [[MarkedAsTransient]] tagged constructors in the [[ToConstructorMap]]
+   */
   trait ToConstructorMapLowPriority {
     implicit def clist[K <: Symbol, H, T <: Coproduct](implicit witness: Witness.Aux[K],
                                                        tail: ToConstructorMap[T]): ToConstructorMap[FieldType[K, H] :+: T] =
@@ -187,6 +256,13 @@ trait GenericDerivationApi extends LowerPriorityGenericDerivationApi {
         val constructors: Vector[String] = witness.value.name +: tail.constructors
       }
   }
+
+  /**
+   * Implementation of the [[ToConstructorMap]] type class for both products and coproducts.
+   *
+   * For products the result is always an empty vector.
+   * For coproducts the result if the vector of constructor names, except the ones marked as transient.
+   */
   object ToConstructorMap extends ToConstructorMapLowPriority {
     implicit val cnil: ToConstructorMap[CNil] = new ToConstructorMap[CNil] { val constructors: Vector[String] = Vector.empty }
     implicit def clistTransient[K <: Symbol, H, T <: Coproduct](implicit witness: Witness.Aux[K],
@@ -198,6 +274,49 @@ trait GenericDerivationApi extends LowerPriorityGenericDerivationApi {
     implicit def hlist[H <: HList]: ToConstructorMap[H] = new ToConstructorMap[H] { val constructors: Vector[String] = Vector.empty }
   }
 
+  /**
+   * The entry point of deriving a generic binary codec for a type 'T'
+   *
+   * The derivation needs both type level and runtime information about the type it derives the codec for.
+   * On type level first the [[LabelledGeneric]] representation gets extracted into 'H'. This is either a [[HList]]
+   * of fields or a [[Coproduct]] of constructors. Then with [[Annotations]] it finds both the [[TransientField]]
+   * and [[TransientConstructor]] annotations for each field and constructor, stored in the 'Trs' and 'Trcs' types.
+   * Based on these the [[TagTransients]] type level operation constructs 'TH' where each field/constructor is
+   * tagged with its label and optionally as transient. This is the type which is then used recursively to
+   * generate the serializer and the deserializer through the implicits of [[ChunkedBinarySerializer]] and
+   * [[ChunkedBinaryDeserializer]].
+   *
+   * For these serializer and deserializer operations we also need some runtime data. The [[ToConstructorMap]]
+   * type class extracts the name of non-transient constructors into a runtime [[Vector]]. This defines the
+   * association between constructors and numeric IDs.
+   *
+   * It is not enough to have transient tags in type level because the [[TransientField]] annotation also holds
+   * a default value. For this reason we need to be able to access the default value runtime as well, during
+   * the construction of the generic representation in the deserializer.
+   *
+   * With 'keys', 'zip' and 'toList' we zip together the field names with the annotations and convert the result
+   * to a runtime [[Map]].
+   *
+   * @param gen Extracts the labelled generic representation of type 'T' into 'H'
+   * @param keys Gets the HList of field/constructor names of 'H' into 'Ks'
+   * @param transientAnnotations Extracts the [[TransientField]] annotations of 'T' into 'Trs'
+   * @param transientConstructorAnnotations Extracts the [[TransientConstructor]] annotations of 'T' into 'Trcs'
+   * @param taggedTransients Tags the generic representation 'H' with transient tags based on 'Trs' and 'Trcs' into 'TH'
+   * @param zip Creates a zipped list of 'Ks' and 'Trs', associating field names with transient annotations into 'KsTrs'
+   * @param toList Extractor of the 'KsTrs' [[HList]] into a [[List]] of [[Symbol]] and optional [[TransientField]] pairs
+   * @param serializer The serializer implementation for the tagged generic representation 'TH'
+   * @param deserializer The deserializer implementation for the tagged generic representation 'TH'
+   * @param toConstructorMap Extracts the names of the constructors of a coproduct from the generic representation 'TH' into a runtime vector
+   * @param classTag Class tag for the type 'T'
+   * @tparam T Type to derive the generic representation for
+   * @tparam H Labelled generic representation of type 'T'
+   * @tparam Ks [[HList]] of field/constructor names
+   * @tparam Trs [[HList]] of optional [[TransientField]] annotations per field/constructor
+   * @tparam Trcs [[HList]] of optional [[TransientConstructor]] annotations per field/constructor
+   * @tparam KsTrs [[HList]] of pairs of [[Symbol]] and optional [[TransientField]] values
+   * @tparam TH Tagged generic representation of type 'T' produced by [[TagTransients]]
+   * @return The derived [[BinaryCodec]] for type 'T'
+   */
   def derive[T, H, Ks <: HList, Trs <: HList, Trcs <: HList, KsTrs <: HList, TH]
     (implicit gen: LabelledGeneric.Aux[T, H],
      keys: Lazy[Symbols.Aux[H, Ks]],
@@ -206,8 +325,8 @@ trait GenericDerivationApi extends LowerPriorityGenericDerivationApi {
      taggedTransients: TagTransients.Aux[H, Trs, Trcs, TH],
      zip: Zip.Aux[Ks :: Trs :: HNil, KsTrs],
      toList: ToTraversable.Aux[KsTrs, List, (Symbol, Option[TransientField])],
-     hlistSerializer: Lazy[ChunkedBinarySerializer[TH]],
-     hlistDeserializer: Lazy[ChunkedBinaryDeserializer[TH]],
+     serializer: Lazy[ChunkedBinarySerializer[TH]],
+     deserializer: Lazy[ChunkedBinaryDeserializer[TH]],
      toConstructorMap: Lazy[ToConstructorMap[TH]],
      classTag: ClassTag[T]): BinaryCodec[T]
 }
@@ -459,8 +578,8 @@ class GenericBinaryCodec(evolutionSteps: Vector[Evolution]) extends GenericDeriv
    taggedTransients: TagTransients.Aux[H, Trs, Trcs, TH],
    zip: Zip.Aux[Ks :: Trs :: HNil, KsTrs],
    toList: ToTraversable.Aux[KsTrs, List, (Symbol, Option[TransientField])],
-   hlistSerializer: Lazy[ChunkedBinarySerializer[TH]],
-   hlistDeserializer: Lazy[ChunkedBinaryDeserializer[TH]],
+   serializer: Lazy[ChunkedBinarySerializer[TH]],
+   deserializer: Lazy[ChunkedBinaryDeserializer[TH]],
    toConstructorMap: Lazy[ToConstructorMap[TH]],
    classTag: ClassTag[T]): BinaryCodec[T] = {
     val constructorMap = toConstructorMap.value.constructors
@@ -490,7 +609,7 @@ class GenericBinaryCodec(evolutionSteps: Vector[Evolution]) extends GenericDeriv
             transientFields
           )
           result <- Ser.fromEither(
-            hlistSerializer.value.serialize(taggedTransients.tag(genericValue))
+            serializer.value.serialize(taggedTransients.tag(genericValue))
               .run(chunkedOutput)
               .run(initialState))
           (finalState, _) = result
@@ -515,7 +634,7 @@ class GenericBinaryCodec(evolutionSteps: Vector[Evolution]) extends GenericDeriv
           typeDescription = classTag.runtimeClass.getName,
           readConstructorName = None,
           transientFields)
-        result <- Deser.fromEither(hlistDeserializer.value.deserialize()
+        result <- Deser.fromEither(deserializer.value.deserialize()
           .run(chunkedInput)
           .run(initialState))
         (finalState, hlist) = result
@@ -646,6 +765,9 @@ class GenericBinaryCodec(evolutionSteps: Vector[Evolution]) extends GenericDeriv
 }
 
 object GenericBinaryCodec {
+  /**
+   * A common static instance of the binary codec for all types not having any evolution steps
+   */
   val simple = new GenericBinaryCodec(Vector(InitialVersion))
 
   trait TransientTag[T]
