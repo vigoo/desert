b425723cec333fab58db2404a4a3ee0995eda343
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-07-27 19:41:49 +0200

    Migrate to GitHub Actions

--- scripts/decrypt_keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -x
-
-openssl aes-256-cbc -K $encrypted_05f822b6caa8_key -iv $encrypted_05f822b6caa8_iv -in travis-deploy-key.enc -out travis-deploy-key -d
-chmod 600 travis-deploy-key
-cp travis-deploy-key ~/.ssh/id_rsa
